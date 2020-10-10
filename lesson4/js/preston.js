7a9a2b28214f9baba14387f2775feea59b16c5a2
DJBarBar <coryjbarton@gmail.com>
2020-10-10 17:37:39 -0600

    lesson 4

---
+++ lesson4/js/preston.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("hide");
+}
+const options = { dateStyle: "full" };
+document.getElementById(
+  "current-date"
+).textContent = new Date().toLocaleDateString("en-US", options);
