2c1be25d769e280aa607197887621aff38da1f4d
DJBarBar <coryjbarton@gmail.com>
2020-10-28 11:48:16 -0600

    Fixes to lessons 4,5,&6

--- lesson5/js/preston.js
+++ lesson5/js/preston.js
@@ -1,5 +1,14 @@
-const options = { year: "numeric" };
-document.getElementById(
-  "current-date"
-).textContent = new Date().toLocaleDateString("en-US", options);
-document.getElementById("last-updated").innerHTML = document.lastModified;
+function toggleMenu() {
+  document.getElementsByClassName("navigate")[0].classList.toggle("responsive");
+}
+
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+};
+document.getElementById("current-date").textContent = new Date().toLocaleString(
+  "en-GB",
+  options
+);
