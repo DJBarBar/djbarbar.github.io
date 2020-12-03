db4ebd87f2c96dc8c58903f6bcd7375d89d688ed
DJBarBar <coryjbarton@gmail.com>
2020-12-02 21:21:20 -0700

    Preston update

--- lesson10/js/master.js
+++ lesson10/js/master.js
@@ -1,81 +1,72 @@
-//navigation
+// Responsive Navigation Function
 function toggleMenu() {
-  document.getElementById("primaryNav").classList.toggle("hide");
+  document
+    .getElementsByClassName("navigation")[0]
+    .classList.toggle("responsive");
 }
 
-//Current Date
+//Current Date Script
 const options = {
   weekday: "long",
   day: "numeric",
   month: "long",
   year: "numeric",
 };
-document.getElementById("current-date").textContent = new Date().toLocaleString(
+document.getElementById("currentdate").textContent = new Date().toLocaleString(
   "en-GB",
   options
 );
 
-//Pancake
+//Event Header
 new Date().getDay() == 5
-  ? (document.getElementById("pancake").innerHTML =
-      "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.")
-  : (document.getElementById("pancake").style.display = "block");
+  ? (document.getElementById("party").innerHTML =
+      "Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.")
+  : (document.getElementById("party").style.display = "block");
 
-//Wind Chill
-window.onload = function windChill() {
-  let temp = parseFloat(document.getElementById("currTemp").textContent);
-  let speed = parseFloat(document.getElementById("windSpeed").textContent);
-  let output = "N/A";
-  if (temp <= 50 && speed >= 3) {
-    let f =
-      35.74 +
-      0.6215 * temp -
-      35.75 * Math.pow(speed, 0.16) +
-      0.4275 * (temp * Math.pow(speed, 0.16));
-    output = Math.round(f);
+//Get current year
+var yearnow = new Date();
+document.getElementById("thisyear").innerHTML = yearnow.getFullYear();
+
+//Web Font Load API
+WebFont.load({
+  google: {
+    families: ["Gudea", "Acme"],
+  },
+});
+
+//Lazy Loading Images Script
+const images = document.querySelectorAll("img[data-src]");
+
+function preloadImage(img) {
+  const src = img.getAttribute("data-src");
+  if (!src) {
+    return;
+  } else {
+    img.src = src;
   }
-  document.getElementById("chill").innerHTML = output + " &#8457;";
-};
-
-//Image Handleing
-const imagesToLoad = document.querySelectorAll("img[data-src]");
+}
 
 const imgOptions = {
-  threshold: 0.5,
+  threshold: 0,
   rootMargin: "0px 0px 50px 0px",
 };
 
-const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
-  image.onload = () => {
-    image.removeAttribute("data-src");
-  };
-};
-
-if ("IntersectionObserver" in window) {
-  const observer = new IntersectionObserver((items, observer) => {
-    items.forEach((item) => {
-      if (item.isIntersecting) {
-        loadImages(item.target);
-        observer.unobserve(item.target);
-      }
-    });
-  }, imgOptions);
-  imagesToLoad.forEach((img) => {
-    observer.observe(img);
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImage(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
   });
-} else {
-  imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
-}
+}, imgOptions);
 
-//Slider
-function adjustSlider(sslider) {
-  document.getElementById("slidervalue").innerHTML = sslider;
-}
+images.forEach((image) => {
+  imgObserver.observe(image);
+});
 
-//City Info
+//City Information API Load
 fetch("https://byui-cit230.github.io/weather/data/towndata.json")
   .then(function (response) {
     return response.json();
@@ -85,6 +76,7 @@ fetch("https://byui-cit230.github.io/weather/data/towndata.json")
     const arrCity = ["Fish Haven", "Preston", "Soda Springs"];
     for (let i = 0; i < cities.length; i++) {
       if (arrCity.includes(cities[i].name)) {
+        let plink = document.createElement("a");
         let card = document.createElement("section");
         let info = document.createElement("div");
         let name = document.createElement("h2");
@@ -114,8 +106,111 @@ fetch("https://byui-cit230.github.io/weather/data/towndata.json")
         card.appendChild(name);
         card.appendChild(motto);
         card.appendChild(info);
+        plink.appendChild(card);
+        plink.setAttribute("class", "plink");
+        plink.setAttribute(
+          "href",
+          "pages/" + cities[i].name.replace(/\s/g, "_").toLowerCase() + ".html"
+        );
 
-        document.querySelector("div.cityinfo").appendChild(card);
+        document.querySelector("div.cityinfo").appendChild(plink);
+      }
+      const cname = document
+        .getElementsByName("city_name")[0]
+        .getAttribute("content");
+      for (var i2 = 0; i2 < cities.length; i2++) {
+        if (cname == cities[i2].name) {
+          for (var j = 0; j < cities[i2].events.length; j++) {
+            var cevents = document.createElement("li");
+            var nevent = document.createTextNode(cities[i2].events[j]);
+            cevents.appendChild(nevent);
+            document.querySelector("ul.event_list").appendChild(cevents);
+          }
+        }
       }
     }
   });
+
+//Weather demographics to be set by dynamic load off of meta tags for city page
+var cc = document.getElementsByName("city_code")[0].getAttribute("content");
+var CurrentWeatherURL =
+  "https://api.openweathermap.org/data/2.5/weather?id=" +
+  cc +
+  "&APPID=44816e5905baa4f18d6261ad8ccb925a&units=imperial";
+var ForeCastWeatherURL =
+  "https://api.openweathermap.org/data/2.5/forecast?id=" +
+  cc +
+  "&APPID=44816e5905baa4f18d6261ad8ccb925a&units=imperial";
+
+fetch(CurrentWeatherURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    document.getElementById("ccondition").innerHTML = jsObject.weather[0].main;
+    document.getElementById("ctemp").innerHTML =
+      jsObject.main.temp.toFixed(0) + " &#8457";
+    document.getElementById("htemp").innerHTML =
+      jsObject.main.temp_max.toFixed(0) + " &#8457";
+    document.getElementById("humid").innerHTML = jsObject.main.humidity + " %";
+    document.getElementById("wspeed").innerHTML =
+      jsObject.wind.speed.toFixed(0) + " mph";
+
+    var ct = parseFloat(document.getElementById("ctemp").textContent);
+    var ws = parseFloat(document.getElementById("wspeed").textContent);
+
+    if (isNaN(ct) || isNaN(ws) || ct >= 70) {
+      document.getElementById("wchill").innerHTML = "N/A";
+    } else {
+      document.getElementById("wchill").innerHTML =
+        windchill(ct, ws) + " &#8457";
+    }
+  });
+
+fetch(ForeCastWeatherURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    for (i = 0; i < 3; i++) {
+      let trow = document.createElement("tr");
+      trow.setAttribute("class", "row" + (i + 1));
+      document.querySelector("tbody").appendChild(trow);
+    }
+    for (j = 0; j < jsObject.list.length; j++) {
+      if (jsObject.list[j].dt_txt.includes("18:00:00")) {
+        let thcell = document.createElement("th");
+        let ttcell = document.createElement("td");
+        let ticell = document.createElement("td");
+        let images = document.createElement("img");
+
+        let fdate = new Date(jsObject.list[j].dt_txt);
+        let wday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+        images.setAttribute(
+          "src",
+          "https://openweathermap.org/img/w/" +
+            jsObject.list[j].weather[0].icon +
+            ".png"
+        );
+        images.setAttribute("alt", "jsObject.list[j].weather[0].description");
+        thcell.textContent = wday[fdate.getDay()];
+
+        ticell.appendChild(images);
+        ttcell.innerHTML = jsObject.list[j].main.temp.toFixed(0) + " &#8457";
+
+        document.querySelector("tr.row1").appendChild(thcell);
+        document.querySelector("tr.row2").appendChild(ticell);
+        document.querySelector("tr.row3").appendChild(ttcell);
+      }
+    }
+  });
+
+//wind chill calculator
+function windchill(tempF, speed) {
+  return Math.round(
+    Math.round(
+      100 *
+        (35.74 +
+          0.6215 * tempF -
+          35.75 * speed ** 0.16 +
+          0.4275 * tempF * speed ** 0.16)
+    ) / 100
+  );
+}
