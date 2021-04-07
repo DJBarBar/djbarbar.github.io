bb1a26e6aeba27048d12de1d5d75932c285eb121
DJBarBar <coryjbarton@gmail.com>
2021-04-06 21:45:16 -0600

    week 7

--- lesson7/js/imagehandling.js
+++ lesson7/js/imagehandling.js
@@ -2,27 +2,25 @@ const imagesToLoad = document.querySelectorAll("img[data-src]");
 
 const imgOptions = {
   threshold: 0.5,
-  rootMargin: "0px 0px 50px 0px",
 };
-
 const loadImages = (image) => {
   image.setAttribute("src", image.getAttribute("data-src"));
   image.onload = () => {
-    image.removeAttribute("data-src");
+    image.removeAttribute("date-src");
   };
 };
-
 if ("IntersectionObserver" in window) {
-  const observer = new IntersectionObserver((items, observer) => {
+  const imgObserver = new IntersectionObserver((items, imgObserver) => {
     items.forEach((item) => {
       if (item.isIntersecting) {
         loadImages(item.target);
-        observer.unobserve(item.target);
+        imgObserver.unobserve(item.target);
       }
     });
   }, imgOptions);
+
   imagesToLoad.forEach((img) => {
-    observer.observe(img);
+    imgObserver.observe(img);
   });
 } else {
   imagesToLoad.forEach((img) => {
